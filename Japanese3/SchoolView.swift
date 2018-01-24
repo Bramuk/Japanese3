443254b9ea67035c675bc484df2d1d18154ea7bd
박지원 <jiwon@bagjiwonui-MacBook-Air.local>
2018-01-24 09:33:41 +0900

    school view added

---
+++ Japanese3/SchoolView.swift
@@ -0,0 +1,18 @@
+//
+//  SchoolView.swift
+//  Japanese3
+//
+//  Created by 박지원 on 23/01/2018.
+//  Copyright © 2018 Bram Williams. All rights reserved.
+//
+
+import Cocoa
+
+class SchoolView: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
