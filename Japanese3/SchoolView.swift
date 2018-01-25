9d9de2012b06a040ffbdc90c8c6c9adfdf38ee15
박지원 <jiwon@bagjiwonui-MacBook-Air.local>
2018-01-26 00:37:07 +0900

    School Question 1 Buttons and Answer Popups

--- Japanese3/SchoolView.swift
+++ Japanese3/SchoolView.swift
@@ -12,7 +12,39 @@ class SchoolView: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ScQ1WrgAnsPopUp.isHidden = true
+        ScQ1CrtAnsPopUp.isHidden = true
+
         // Do view setup here.
     }
+    
+    
+    @IBOutlet weak var ScQ1WrgAnsPopUp: NSImageView!
+    
+    @IBOutlet weak var ScQ1CrtAnsPopUp: NSImageView!
+    
+    @IBAction func ScQ1TLButton(_ sender: Any)
+    {
+        ScQ1WrgAnsPopUp.isHidden = false
+    }
+    
+    
+    @IBAction func ScQ1TRButton(_ sender: Any)
+    {
+        ScQ1WrgAnsPopUp.isHidden = false
+    }
+    
+    @IBAction func ScQ1BLButton(_ sender: Any)
+    {
+        ScQ1CrtAnsPopUp.isHidden = false
+    }
+    
+    @IBAction func ScQ1BRButton(_ sender: Any)
+    {
+        ScQ1WrgAnsPopUp.isHidden = false
+    }
+    
+    
     
 }
