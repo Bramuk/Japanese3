31bb20c1c359ec1e6186f6a7054c1f5cf754e395
Bram Williams <bramwilliams@brams-macbook-pro.local>
2018-02-13 10:07:54 +0900

    February 13 10:07

---
+++ Japanese3-master/Japanese3/FestivalView1.swift
@@ -0,0 +1,18 @@
+//
+//  FestivalView1.swift
+//  Japanese3
+//
+//  Created by Bram Williams on 2018/02/13.
+//  Copyright © 2018 Bram Williams. All rights reserved.
+//
+
+import Cocoa
+
+class FestivalView1: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
