63e6c87b8467fbe7ae0f7319b80cd058b5c5ee71
Kotomi T <kotomit@kotomis-macbook-air.local>
2018-02-18 11:12:33 +0900

    February 18, 11:12 WE FINISHED!!

---
+++ Japanese3/LastSc.swift
@@ -0,0 +1,18 @@
+//
+//  LastSc.swift
+//  Japanese3
+//
+//  Created by Kotomi T on 2018/02/18.
+//  Copyright © 2018 Bram Williams. All rights reserved.
+//
+
+import Cocoa
+
+class LastSc: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
